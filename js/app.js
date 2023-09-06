2d0c17c8bf7d418d1a1402c3d5d9675be8f71001
Ezequiel Bernard <99434175+ezequielbernard@users.noreply.github.com>
2023-09-06 19:40:57 -0300

    mpnocors

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('form');
 const btn = document.getElementById('button');
 const emailInput = document.getElementById('email_id');
-const cors = require("cors");
 
 document.getElementById("form")
  .addEventListener('submit', function(event) {
